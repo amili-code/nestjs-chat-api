d2fef5f80998cb419f9ed304da25697c8ec5e44f
amili-code <a.ghorbani.info@gmail.com>
2025-01-29 18:33:30 +0330

    user servis update

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,11 +6,11 @@ import { User } from './user.entity';
 @Injectable()
 export class UsersService {
   constructor(
-    @InjectRepository(User) private readonly userRepository: Repository<User>,
+    @InjectRepository(User) private readonly usersRepository: Repository<User>,
   ) {}
 
   async validateUser(username: string, password: string): Promise<User | null> {
-    const user = await this.userRepository.findOne({ where: { username } });
+    const user = await this.usersRepository.findOne({ where: { username } });
     if (user && user.password === password) {
       // اینجا باید bcrypt استفاده بشه
       return user;
