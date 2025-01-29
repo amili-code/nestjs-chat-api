5cd9c9d1ab0fe90b6180a0fcea02b768c0f0e553
amili-code <a.ghorbani.info@gmail.com>
2025-01-29 18:26:31 +0330

    jwt full

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -9,7 +9,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'secret', // اینم از .env بیار
+      secretOrKey: 'mySecretKey', // اینم از .env بیار
     });
   }
 
